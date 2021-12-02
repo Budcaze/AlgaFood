a1b051a33573bacb92bfcd2c5d930fa685a7f417
Eduardo Henrique Cazé de Almeida <66096920+Budcaze@users.noreply.github.com>
2021-12-02 01:00:35 -0300

    commit

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/TestControlleer.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/TestControlleer.java
@@ -51,5 +51,10 @@ public class TestControlleer {
     public List<Restaurante> restaurantesTop2(String nome) {
         return restauranteRepository.findTop2ByNomeContaining(nome);
     }
+    @GetMapping("/restaurantes/por-nome-e-frete")
+    public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial,
+    BigDecimal taxaFreteFinal){
+        return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+    }
 
 }
