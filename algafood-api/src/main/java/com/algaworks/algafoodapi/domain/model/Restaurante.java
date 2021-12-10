64cafc7e9ab06cb53684d10f9cc171a3678bf6e0
Eduardo Henrique Cazé de Almeida <66096920+Budcaze@users.noreply.github.com>
2021-12-10 00:58:11 -0300

    6.3

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -26,6 +26,7 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)//Caso eu queira alterar o nome
     private Cozinha cozinha;
+    @JsonIgnore
     @ManyToMany //Muitos Restaurantes para muitos formaPagamento
     @JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"),
     inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
